2209c0d597b43fef683900c50e4e8226c4cf0353
IronMan988 <rodrigoff988@gmail.com>
2021-05-03 11:23:31 +0100

    udp connection between client and server

--- client.c
+++ client.c
@@ -1,16 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #define MAX_LINE 100
 
+#define PORT 9876
+
 int main(int argc, char** argv) {
+    struct sockaddr_in serv_addr;
+    socklen_t slen = sizeof(serv_addr);
+    int s;
 
     if (argc != 3) {
         printf("client <server address> <port>\n");
         exit(-1);
     }
 
+    // UDP connection to server
+    // Cria um socket para recepção de pacotes UDP
+	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		perror("Erro na criação do socket");
+	}
+
+    // Preenchimento da socket address structure
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // test udp connection
+    char line[MAX_LINE];
+
+    while(1) {
+        fgets(line, 10, stdin);
+        line[strlen(line)-1] = '\0';
+        sendto(s, line, strlen(line), 0, (struct sockaddr *) &serv_addr, (socklen_t ) slen);
+        printf("Send to server: %s\n", line);
+    }
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,15 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define MAX_LINE 100
 #define MAX_INFO 30
 
-typedef struct {
+#define PORT 9876       // server port
+
+typedef struct user_info{
     char userID[MAX_INFO];
 } user_info;
 
 int main(int argc, char** argv) {
+    struct sockaddr_in si_minha, si_outra;
+    socklen_t slen = sizeof(si_outra);
+	int s, recv_len;
 
     if (argc != 4) {
         printf("server <port clients> <port config> <log file>\n");
@@ -17,12 +24,34 @@ int main(int argc, char** argv) {
     }
 
     // initialize UDP connection
+    // Cria um socket para recepção de pacotes UDP
+	if (( s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		perror("Erro na criação do socket");
+	}
+
+    // Preenchimento da socket address structure
+	si_minha.sin_family = AF_INET;
+	si_minha.sin_port = htons(PORT);
+	si_minha.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Associa o socket à informação de endereço
+	if (bind(s,(struct sockaddr*)&si_minha, sizeof(si_minha)) == -1) {
+		perror("Erro no bind");
+	}
+
 
     // initialize TCP connection
 
     char line[MAX_LINE];
 
     while(1) {
+        if((recv_len = recvfrom(s, line, MAX_LINE, 0, (struct sockaddr *) &si_outra, (socklen_t *)&slen)) == -1) {
+	        perror("Erro no recvfrom");
+	    }
+        printf("Received: %s\n", line);
+    }
+
+    /*while(1) {
         fgets(line, 10, stdin);
         line[strlen(line)-1] = '\0';
 
@@ -45,7 +74,7 @@ int main(int argc, char** argv) {
             continue;
         }
 
-    }
+    }*/
 
     return 0;
 }
