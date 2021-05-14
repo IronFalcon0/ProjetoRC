b9d7c3ae7708e6b2eb6f4f5dbe5e7eb0370cf361
jogui2k <joguildo321@gmail.com>
2021-05-14 21:05:06 +0000

    arrumei
    uwu

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <pthread.h>
 #include <signal.h>
 #include <unistd.h>
+#include <ctype.h>
 // #include <sys/types.h>
 // #include <arpa/inet.h>
 // #include <netdb.h>
@@ -61,7 +62,7 @@ void sigint (int sig_num) {
     exit(0);
 }
 
-void get_info(char *);
+bool get_info(char *);
 void load_info();
 void printa(user_info *);
 int find_user(char [], char []);
@@ -87,18 +88,28 @@ int main(int argc, char** argv){
     // initialize TCP connection
     bzero((void *) &serv_config_addr, sizeof(serv_config_addr));
     serv_config_addr.sin_family = AF_INET;
-    serv_config_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    inet_aton("127.0.0.1", &serv_config_addr.sin_addr);
+    //serv_config_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_config_addr.sin_port = htons(atoi(argv[2]));
 
-    if ((fd_tcp = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((fd_tcp = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    printf("oi\n");
 		perror("na funcao socket");
+    exit(0);
+    }
 
-    if ( bind(fd_tcp, (struct sockaddr*) &serv_config_addr,sizeof(serv_config_addr)) < 0)
+    if ( bind(fd_tcp, (struct sockaddr*) &serv_config_addr,sizeof(serv_config_addr)) < 0){
+    printf("ola\n");
 		perror("na funcao bind");
+    exit(0);
+    }
 
     // wait for connections
-    if( listen(fd_tcp, 5) < 0)
+    if( listen(fd_tcp, 5) < 0){
+    printf("vadia\n");
 		perror("na funcao listen");
+    exit(0);
+    }
 
     // create thread to handle with config users
     pthread_create(&config_thread, NULL, config_users, NULL);
@@ -108,6 +119,7 @@ int main(int argc, char** argv){
     // socket to receive UDP packages from clients
 	if ((s_clients = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
 		perror("Erro na criação do socket");
+    exit(0);
 	}
 
 	serv_clients_addr.sin_family = AF_INET;
@@ -117,13 +129,14 @@ int main(int argc, char** argv){
     // bind socket
 	if (bind(s_clients, (struct sockaddr*)&serv_clients_addr, sizeof(serv_clients_addr)) == -1) {
 		perror("Erro no bind");
+    exit(0);
 	}
 
 
     // print server info
     char server_addr[30];
-	inet_ntop(AF_INET, &(serv_clients_addr.sin_addr), server_addr, INET_ADDRSTRLEN);
-    printf("server address: %s\n UDP port: %d\n TCP port: %d\n", server_addr, ntohs(serv_clients_addr.sin_port), ntohs(serv_config_addr.sin_port));
+	  inet_ntop(AF_INET, &(serv_clients_addr.sin_addr), server_addr, INET_ADDRSTRLEN);
+    printf("server address: %s\n UDP port: %d\n TCP port: %d\n", inet_ntoa(serv_config_addr.sin_addr), ntohs(serv_clients_addr.sin_port), ntohs(serv_config_addr.sin_port));
 
 
     // clients autentication
@@ -165,42 +178,65 @@ int find_user(char userName[MAX_INFO], char password[MAX_INFO]) {
     }
     return 0;
 }
+bool valid_ip(char* ip){
+  int n_p = 0, n_v = 0, i = 0;
+  while(ip[i] != 0){
+    if(ip[i] == '.' && (n_v == 0 || n_v > 3))return false;
+    if (isdigit(ip[i]))n_v++;
+    else if (ip[i] == '.')n_p++, n_v = 0;
+    i++;
+  }
+  return (n_p == 3 && (n_v >= 1 && n_v <= 3));
+}
 
-
-void get_info(char *str){
+bool get_info(char *str){
     char *tok;
     user_info user;
     int count = 0;
-    tok = strtok(str," ");
+    char aux[MAX_LINE];
+    strcpy(aux,str);
+    tok = strtok(aux," ");
     strcpy(user.userName,tok);
-
+    //printf("%s\n", user.userName);
     for(count = 0; tok != NULL;){
         tok = strtok(NULL, " ");
 
         if(tok != NULL){
-            if(count == 0) strcpy(user.ip,tok);
-            if(count == 1) strcpy(user.password,tok);
-            if(count == 2){
+            if((count == 0) && valid_ip(tok)) strcpy(user.ip,tok);
+            else if(count == 1) strcpy(user.password,tok);
+            else if(count == 2){
+                if((tok[0] != '0' && tok[0] != '1') ||(tok[1] != '0'&&tok[1] != '1')||(tok[2] != '0'&&tok[2]!='1')){
+                  printf("ERRO de formatacao\n");
+                  return false;
+                }
                 user.client_server = tok[0] - '0';
                 user.p2p = tok[1] - '0';
                 user.group = tok[2] - '0';
+            }
+            else{
+              printf("ERRO de formatacao\n");
+              return false;
             }
             count ++;
         }
     }
 
     if(count != 3){
+        printf("%d\n", count);
         printf("ERRO de formatacao\n");
+        return false;
     }else{
         for(int i=0; i< n_users; i++){
             if(strcmp(user.userName,users[i].userName) == 0){
                 printf("Username %s is already in use\n",user.userName);
-                return;
+                return false;
             }
         }
         users[n_users] = user;
         n_users++;
     }
+    printf("cabei\n");
+    return true;
 
 }
 
@@ -216,12 +252,8 @@ void load_info(){
         printf("ERRO AO ABRIR FICHEIRO\n");
 
     while(fgets(line, MAX_LINE, fp) != NULL) {
-        char *tok = strtok(line,"\n");
-
-        while(tok != NULL){
-            get_info(tok);
-            tok = strtok(NULL, "\n");
-        }
+            get_info(line);
+            printf("%s\n", line);
     }
     fclose(fp);
 }
@@ -248,13 +280,18 @@ int find_username(char name[]){
     }
     return -1;
 }
+void trim(char* str){
+      int i = strlen(str)-1;
+      while(isspace(str[i])) str[i--] = 0;
+}
 
 void remove_from_file(char name[]){
-    char *aux[n_users];
+    char aux[n_users][MAX_LINE];
     char line[MAX_LINE];
     FILE *fp;
     int i = 0;
     char *tok;
+    char aux2[MAX_LINE];
     fp = fopen(file_name, "r");
     if(fp == NULL){
         printf("ERROR: CANNOT OPEN FILE\n");
@@ -263,13 +300,17 @@ void remove_from_file(char name[]){
     while(fgets(line, MAX_LINE, fp) != NULL){
         //char *tok = strtok(line,"\n");
         //while(tok != NULL){
-        tok = strtok(line, " ");
+        strcpy(aux2,line);
+        tok = strtok(aux2, " ");
         if(strcmp(tok,name)==0){
             continue;
         }
-        aux[i++] = line;
+        strcpy(aux[i],line);
+        i++;
+        printf("%s\n", aux[i-1]);
             //tok = strtok(NULL, "\n");
     }
+
     fclose(fp);
     fp = fopen(file_name,"w");
     if(fp == NULL){
@@ -277,57 +318,86 @@ void remove_from_file(char name[]){
         return;
     }
     for(int j =0; j<i;j++){
-        fprintf(fp,"%s",aux[j]);
+        fputs(aux[j],fp);
     }
     fclose(fp);
 
+}
+
+bool split(char *input, char *st, char separator){
+    if(input[0] == 0) return false;
+
+    char string[strlen(input)];
+    int i=0,j=0;
+    while((input[i] != separator) && input[i] != 0){
+      st[i] = input[i];
+      i++;
+    }
+    st[i] =0;
+    while(input[i] == separator) i++;
+    while(input[i] != 0) string[j++] = input[i++];
+
+    string[j] = 0;
+    strcpy(input,string);
+    return true;
 }
 
 
 void *config_users(void* i) {
     signal(SIGUSR1, sigusr1);
     printf("Thread initialized\n");
-
+    char st[MAX_LINE];
     int conf = 0, nread = 0;
     char line[MAX_LINE];
     conf = accept(fd_tcp,(struct sockaddr *) &serv_config_addr,(socklen_t *)&config_size);
     printf("to dps do conf\n");
-    while(waitpid(-1,NULL,WNOHANG)>0);
+    //while(waitpid(-1,NULL,WNOHANG)>0);
     if (conf <= 0) return NULL;
     printf("to dps do segundo conf\n");
     while(1) {
-        nread = read(fd_tcp, line, MAX_LINE-1);
+        nread = read(conf, line, MAX_LINE-1);
+        printf("cacete\n");
         if(nread == -1)perror("ERROR");
         printf("to na 299\n");
-        char *tok = strtok(line," ");
-        char *aux2 = strtok(NULL, "\0");
-        if (nread != 0) {
+        if (nread != -1) {
             line[nread] = 0;
             printf("Received config: %s", line);
             // commands TODO
-            if(strcasecmp(line, "list") == 0){
+            trim(line);
+            split(line, st, ' ');
+            printf("%s\n", st);
+            if(strcasecmp(st, "list") == 0){
                 int i;
                 for(i=0; i < n_users ; i++ ){
-                    printf("User-id => %s\nIP => %s\nPassword => %s\nClient-Server => %c\nP2p => %c\nGrupo => %c\n\n",
+                    printf("User-id => %s\nIP => %s\nPassword => %s\nClient-Server => %d\nP2p => %d\nGrupo => %d\n\n",
                     users[i].userName,users[i].ip,users[i].password,users[i].client_server,users[i].p2p,users[i].group);
                 }
             }
-            else if(strcasecmp(tok,"add") == 0){
-                get_info(aux2);
-                write_on_file(aux2);
+            else if(strcasecmp(st,"add") == 0){
+                printf("bila bilu\n");
+                if(!get_info(line)) continue;
+                write_on_file(line);
+                printf("%s\n",line);
                 printf("User added successfully\n");
             }
-            else if(strcasecmp(tok,"del")){
+            else if(strcasecmp(st,"del") == 0){
                 int x;
-                if((x=find_username(aux2)) != -1){
+                if((x=find_username(line)) != -1){
                     for(; x< n_users; x++){
                         users[x] = users[x+1];
                     }
                     n_users --;
-                    remove_from_file(aux2);
+                    remove_from_file(line);
                     printf("User removed\n");
                 }
+                else{
+                  printf("User not found\n");
+                }
             }
+            else{
+              printf("COMMAND UNKNOWN\n");
+            }
+
         }
 
       }
